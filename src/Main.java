e648b792fb7497bed717adac848f7ce0f6c80dd6
tommycarale <Tommy.carale12@gmail.com>
2024-09-28 22:47:20 +0800

    F

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,95 @@
 public class Main {
 
-    // 1. parenthesesCheck
-
-
+    public static void main(String[] args) {
+        System.out.println(parenthesesCheck("()"));
+        System.out.println(parenthesesCheck(")(()))"));
+        System.out.println(parenthesesCheck("("));
+        System.out.println(parenthesesCheck("(())((()())())"));
+        System.out.println(reverseInteger(1234));
+        System.out.println(reverseInteger(2468));
+        System.out.println(encryptThis("Hello good day"));
+        System.out.println(encryptThis("Ready set go"));
+        System.out.println(decipherThis("72olle 103doo 100ya"));
+        System.out.println(decipherThis("82yade 115te 103o"));
+    }
+    // 1.parenthesesCheck
+    public static boolean parenthesesCheck(String str) {
+        int openClosed = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '(') {
+                openClosed++;
+            } else if (str.charAt(i) == ')') {
+                openClosed--;
+            }
+            if (openClosed < 0) {
+                return false;
+            }
+        }
+        return openClosed == 0;
+    }
     // 2. reverseInteger
+    public static String reverseInteger(int num) {
+        String number = Integer.toString(num);
+        String reversed = "";
 
+        for (int i = number.length() - 1; i >= 0; i--) {
+            reversed += number.charAt(i);
+        }
+        return reversed;
+    }
+    //3. encryptThis
+    public static String encryptThis(String text) {
+        String[] words = text.split(" ");
+        String encrypted = "";
 
-    // 3. encryptThis
+        for (String word : words) {
+            if (word.length() > 0) {
+                String encryptedWord = (int) word.charAt(0) + "";
+                if (word.length() > 1) {
+                    encryptedWord += word.charAt(word.length() - 1);
+                    if (word.length() > 2) {
+                        encryptedWord += word.substring(2, word.length() - 1);
+                    }
+                    encryptedWord += word.charAt(1);
+                }
 
+                encrypted += encryptedWord + " ";
+            }
+        }
 
-    // 4. decipherThis
+        return encrypted.trim();
+    }
+    //4. decipherThis
+    public static String decipherThis(String text) {
+        String[] words = text.split(" ");
+        StringBuilder deciphered = new StringBuilder();
 
+        for (String word : words) {
+            StringBuilder decipheredWord = new StringBuilder();
+            StringBuilder code = new StringBuilder();
+            int i = 0;
 
+            while (i < word.length() && Character.isDigit(word.charAt(i))) {
+                code.append(word.charAt(i));
+                i++;
+            }
+
+            int firstCharCode = Integer.parseInt(code.toString());
+            char firstChar = (char) firstCharCode;
+            decipheredWord.append(firstChar);
+
+            if (i < word.length()) {
+                char secondChar = word.charAt(i);
+                char lastChar = word.charAt(word.length() - 1);
+                decipheredWord.append(lastChar);
+
+                if (i + 1 < word.length() - 1) {
+                    decipheredWord.append(word, i + 1, word.length() - 1);
+                }
+                decipheredWord.append(secondChar);
+            }
+            deciphered.append(decipheredWord).append(" ");
+        }
+        return deciphered.toString().trim();
+    }
 }
--- src/MainTest.java
+++ src/MainTest.java
@@ -1,7 +1,8 @@
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class  MainTest {
-
+    /**
     @org.junit.jupiter.api.Test
     void parenthesesCheck() {
         assertEquals(true, Main.parenthesesCheck("()"));
@@ -9,6 +10,7 @@ class  MainTest {
         assertEquals(false, Main.parenthesesCheck("("));
         assertEquals(true, Main.parenthesesCheck("(())((()())())"));
     }
+}
 
     @org.junit.jupiter.api.Test
     void reverseInteger() {
@@ -16,13 +18,16 @@ class  MainTest {
         assertEquals("8642", Main.reverseInteger(2468));
         assertEquals("1777", Main.reverseInteger(7771));
     }
+}
 
     @org.junit.jupiter.api.Test
+
     void encryptThis() {
         assertEquals("72olle 103doo 100ya", Main.encryptThis("Hello good day"));
         assertEquals("82yade 115te 103o", Main.encryptThis("Ready set go"));
     }
-
+}
+**/
     @org.junit.jupiter.api.Test
     void decipherThis() {
         assertEquals("Hello good day", Main.decipherThis("72olle 103doo 100ya"));
@@ -30,3 +35,4 @@ class  MainTest {
     }
 
 }
+
